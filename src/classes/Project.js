abdae86da69af0db509787a750e994ec7aafbb02
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-03 13:42:54 -0600

    Add skeleton to project.js class
    Co-authored-by: jacobogart <jacobogart@gmail.com>

---
+++ src/classes/Project.js
@@ -0,0 +1,12 @@
+
+class Project {
+    constructor({project}){
+        this.project_name = project_name;
+        this.palettes = [];
+    }
+
+
+}
+
+//instance of project 
+//query param, fetch projects, get the project id, another method for palettes
