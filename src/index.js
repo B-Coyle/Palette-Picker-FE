ad20e587c9dd0241d88fa3ac59e2b4072ac22ab0
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-08 14:40:21 -0600

    Add beginning functionality for locked and randomize colors
    Co-authored-by: jacobogart <jacobogart@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,62 +1,106 @@
-import Project from './classes/Project';
-import Palette from './classes/Palette';
-import $ from 'jquery';
-import './css/styles.scss';
+import Project from "./classes/Project";
+import Palette from "./classes/Palette";
+import $ from "jquery";
+import "./css/styles.scss";
+import randomHexColor from 'random-hex-color';
+
 let projects = [];
 
-$('#create-project-btn').on('click', (e) => createProject(e))
+$( document ).ready(function() {
+    generateColors();
+});
 
-const baseUrl = 'https://palette-picker-jbbc.herokuapp.com/api/v1/';
+$("#create-project-btn").on("click", e => createProject(e));
+$("#create-palette-btn").on("click", e => createPalette(e));
 
-fetch(baseUrl + 'projects').then(result => {
-    return result.json()
-})
-.then(projectsData => {
+const baseUrl = "https://palette-picker-jbbc.herokuapp.com/api/v1/";
+
+fetch(baseUrl + "projects")
+  .then(result => {
+    return result.json();
+  })
+  .then(projectsData => {
     projectsData.forEach(project => {
-        fetch(baseUrl + `palettes?project_id=${project.id}`).then(result => {
-            return result.json()
+      fetch(baseUrl + `palettes?project_id=${project.id}`)
+        .then(result => {
+          return result.json();
         })
         .then(palettes => {
-            project.palettes = [];
-            palettes.forEach(palette => {
-               project.palettes.push(new Palette(palette))
-            })
-            projects.push(new Project(project));
-            populateOptions(project);
-        })
-    })
-})
+          project.palettes = [];
+          palettes.forEach(palette => {
+            project.palettes.push(new Palette(palette));
+          });
+          projects.push(new Project(project));
+          populateOptions(project);
+        });
+    });
+  });
 
 function createProject(e) {
-    e.preventDefault();
-    const name = $('.project-name-input').val();
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify({project_name: name})
-    }
-    fetch(baseUrl + 'projects', options).then(result => {
-        return result.json()
+  e.preventDefault();
+  const name = $(".project-name-input").val();
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({ project_name: name })
+  };
+  fetch(baseUrl + "projects", options)
+    .then(result => {
+      return result.json();
     })
     .then(data => {
-        const project = {project_name: name, id: data.id}
-        projects.push(new Project(project));
-        populateOptions(project);
-    })
+      const project = { project_name: name, id: data.id };
+      projects.push(new Project(project));
+      populateOptions(project);
+    });
 }
 
 function populateOptions(project) {
-        $('#project-select').append(`<option value=${project.id}>${project.project_name}</option>`)
+  $("#project-select").append(
+    `<option value=${project.id}>${project.project_name}</option>`
+  );
 }
 
 function createPalette(e) {
-    e.preventDefault();
-
-
+  e.preventDefault();
+  const palette = {
+    palette_name: $(".palette-name-input").val(),
+    color1: $("#color1-name").text(),
+    color2: $("#color2-name").text(),
+    color3: $("#color3-name").text(),
+    color4: $("#color4-name").text(),
+    color5: $("#color5-name").text(),
+    project_id: $("#project-select").val()
+  };
+  console.log('palette test', palette);
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-type": "application/json"
+//     },
+//     body: JSON.stringify({ project_name: name })
+//   };
+//   fetch(baseUrl + "projects", options)
+//     .then(result => {
+//       return result.json();
+//     })
+//     .then(data => {
+//       const project = { project_name: name, id: data.id };
+//       projects.push(new Project(project));
+//       populateOptions(project);
+//     });
 }
 
 
-
-
+function generateColors() {
+    $('.color-container').each((index) => {
+        const locked = $(`#color${index+1}`).data().locked
+        if(!locked) {
+            const color = randomHexColor();
+            $(`#color${index+1}`).css('background-color', color);
+            $(`#color${index+1}-name`).text(color);
+        }
+    })
+}
