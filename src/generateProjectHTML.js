bbfc7f714fe3b13a7a917964272eae99f172f7a9
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-09 15:40:50 -0600

    Refactor delete and removed several unused classes
    Co-authored-by: jacobogart <jacobogart@gmail.com>

--- src/generateProjectHTML.js
+++ src/generateProjectHTML.js
@@ -1,10 +1,9 @@
-import generatePaletteHTML from './generatePalleteHTML';
+import generatePaletteHTML from "./generatePaletteHTML";
 
 function generateProjectHTML(project) {
-    const palettes = project.palettes.map(palette => generatePaletteHTML(palette)).join('');
-    console.log()
-    return `
-    <article class='saved-projects' id='project${project.id}'>
+  const palettes = project.palettes.map(palette => generatePaletteHTML(palette)).join('');
+  return `
+  <article class='saved-projects' id='project${project.id}'>
     <h2>${project.project_name}</h2>
     <input type='button' type='button' id=${project.id} class='delete-project-btn' value='Delete Project' />
     ${palettes}
